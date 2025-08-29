7e341b4b3cedc80082bd8866db31799737225081
avazbekm <siddikovavazbek213@gmail.com>
2025-08-29 18:04:13 +0500

    added categoryService

---
+++ VoltStream/src/backend/VoltStream.Application/Features/Categories/Mappers/CategoryMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using VoltStream.Domain.Entities;
+using VoltStream.Application.Features.Categories.DTOs;
+using VoltStream.Application.Features.Categories.Commands;
+
+
+namespace VoltStream.Application.Features.Categories.Mappers;
+
+public class CategoryMappingProfile : Profile
+{
+    public CategoryMappingProfile()
+    {
+        CreateMap<CreateCategoryCommand, Category>();
+        CreateMap<Category, CategoryDto>();
+    }
+}
