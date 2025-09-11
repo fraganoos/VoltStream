97729dec07e1e29562a06ef7f37d6eb2b0b3963c
Yakhya <yahyo1976@gmail.com>
2025-09-11 20:22:23 +0500

    sales

---
+++ VoltStream/src/frontend/VoltStream.WPF/Commons/Utils/StringIsNotEmptyConverter.cs
@@ -0,0 +1,18 @@
+﻿namespace VoltStream.WPF.Commons.Utils;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+public class StringIsNotEmptyConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        // true если строка не пустая
+        return !string.IsNullOrWhiteSpace(value as string);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
