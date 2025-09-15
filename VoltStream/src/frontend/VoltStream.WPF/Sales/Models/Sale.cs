6e228225978059f0fa5ab882e1921e49fbea9b8a
Yakhya <yahyo1976@gmail.com>
2025-09-15 19:47:37 +0500

    Sale model

--- VoltStream/src/frontend/VoltStream.WPF/Sales/Models/Sale.cs
+++ VoltStream/src/frontend/VoltStream.WPF/Sales/Models/Sale.cs
@@ -3,16 +3,25 @@
     public class Sale
     {
         public DateTime OperationDate { get; set; } = DateTime.Now;
+        public long CustomerId { get; set; }
         public string CustomerName { get; set; } = string.Empty;
         public string CurrencyType { get; set; } = string.Empty;
-        public decimal TotalSum { get; set; } // jami summa
-        public decimal TotalDiscount { get; set; } // chegirma
-        public decimal CountRoll { get; set; }    // jami rulonlar soni
-        public decimal TotalQuantity { get; set; } // jami metr
+        public decimal? TotalSum { get; set; } // jami summa
+        public decimal? TotalDiscount { get; set; } // chegirma
+        public bool CheckedDiscount { get; set; } // chegirma ishlatilganmi
+        public decimal? FinalSum { get; set; } // chegirmadan keyingi summa
         public string Description { get; set; } = string.Empty;
-        public string ProductName { get; set; } = string.Empty ;
-        public long CustomerId { get; set; }
-        public long CustomerOperationId { get; set; }
-        public long DiscountOperationId { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+        public long CategoryId { get; set; }
+        public long ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public string PerRollCount { get; set; } = string.Empty; // birligi
+        public decimal? RollCount { get; set; } // rulon
+        public decimal? Quantity { get; set; } // metr
+        public decimal? Price { get; set; } // narxi
+        public decimal? Sum { get; set; } // summa
+        public decimal? PerDiscount { get; set; } // chegirma
+        public decimal? Discount { get; set; } // chegirma summasi
+        public decimal? FinalSumProduct { get; set; } // chegirmadan keyingi narxi
     }
 }
