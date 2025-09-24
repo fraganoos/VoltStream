ba80b900aecb8e1ffaed1fc25840b768401b6034
Yakhya <yahyo1976@gmail.com>
2025-09-24 19:27:12 +0500

    Sale Mijoz

---
+++ VoltStream/src/frontend/VoltStream.WPF/Commons/Utils/InvertibleBooleanToVisibility.cs
@@ -0,0 +1,37 @@
+﻿namespace VoltStream.WPF.Commons.Utils;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+public class InvertibleBooleanToVisibility : IValueConverter
+{
+    public bool Invert { get; set; } = false;
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool boolValue)
+        {
+            if (Invert)
+                boolValue = !boolValue;
+
+            return boolValue ? Visibility.Visible : Visibility.Collapsed;
+        }
+        return Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is Visibility visibility)
+        {
+            bool result = visibility == Visibility.Visible;
+            return Invert ? !result : result;
+        }
+        return false;
+    }
+}
+
