8ffae46aceac294ec7a5113363573fe3047b009a
Yakhya <yahyo1976@gmail.com>
2025-09-25 18:41:48 +0500

    numericText update

--- VoltStream/src/frontend/VoltStream.WPF/Sales/Models/Sale.cs
+++ VoltStream/src/frontend/VoltStream.WPF/Sales/Models/Sale.cs
@@ -1,9 +1,11 @@
 ﻿namespace VoltStream.WPF.Sales.Models
 {
     using System.Collections.ObjectModel;
+    using VoltStream.WPF.Commons;
 
-    public class Sale
+    public class Sale : ViewModelBase
     {
+        public decimal? _finalSum;
         public DateTime OperationDate { get; set; } = DateTime.Now;
         public long CustomerId { get; set; }
         public string CustomerName { get; set; } = string.Empty;
@@ -11,13 +13,21 @@
         public decimal? TotalSum { get; set; } // jami summa
         public decimal? TotalDiscount { get; set; } // chegirma
         public bool CheckedDiscount { get; set; } // chegirma ishlatilganmi
-        public decimal? FinalSum { get; set; } // chegirmadan keyingi summa
+        public decimal? FinalSum
+        {
+            get => _finalSum;
+            set
+            {
+                    _finalSum = value;
+                    OnPropertyChanged(nameof(FinalSum));
+            }
+        }// chegirmadan keyingi summa
         public string Description { get; set; } = string.Empty;
         public string CategoryName { get; set; } = string.Empty;
         public long CategoryId { get; set; }
         public long ProductId { get; set; }
-        public string ProductName { get; set; } = string.Empty;
-        public string PerRollCount { get; set; } = string.Empty; // birligi
+        public string? ProductName { get; set; } 
+        public string? PerRollCount { get; set; }  // birligi
         public string? RollCount { get; set; } // rulon
         public string? Quantity { get; set; } // metr
         public string? Price { get; set; } // narxi
