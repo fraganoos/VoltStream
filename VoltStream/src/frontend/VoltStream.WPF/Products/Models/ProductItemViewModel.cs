bd6ad7a8a7205604c4f7b62dacf11ba3a1dc783a
avazbekm <siddikovavazbek213@gmail.com>
2025-10-08 10:29:16 +0500

    Added productPage

---
+++ VoltStream/src/frontend/VoltStream.WPF/Products/Models/ProductItemViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace VoltStream.WPF.Products.Models;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using VoltStream.WPF.Commons;
+
+public partial class ProductItemViewModel : ViewModelBase
+{
+    [ObservableProperty] private string? category = string.Empty;           // Mahsulot turi
+    [ObservableProperty] private string? name = string.Empty;               // Nomi
+    [ObservableProperty] private decimal? rollLength;                       // Rulon uzunligi
+    [ObservableProperty] private int? quantity;                             // Rulon soni
+    [ObservableProperty] private int? totalCount;                           // Jami (rulon uzunligi * soni)
+    [ObservableProperty] private string? unit = "metr";                     // O‘lchov birligi
+    [ObservableProperty] private decimal? price;                            // Narxi
+    [ObservableProperty] private decimal? totalAmount;                      // Umumiy summa (narx * jami)
+
+}
---
+++ VoltStream/src/frontend/VoltStream.WPF/Products/Views/ProductsPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace VoltStream.WPF.Products.Views;
+using System.Windows.Controls;
+using VoltStream.WPF.Products.Models;
+
+
+/// <summary>
+/// Interaction logic for ProductsPage.xaml
+/// </summary>
+public partial class ProductsPage : Page
+{
+    private ProductViewModel vm;
+    private readonly IServiceProvider services;
+    public ProductsPage(IServiceProvider services)
+    {
+        InitializeComponent();
+        this.services = services;
+        vm = new ProductViewModel(services);
+        DataContext = vm;
+    }
+
+    private void cbxCategory_GotFocus(object sender, System.Windows.RoutedEventArgs e)
+    {
+
+    }
+}
