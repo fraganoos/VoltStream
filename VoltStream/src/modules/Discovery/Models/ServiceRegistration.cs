bed6c35d4a80b01a6a0df21b58006e27eeae9685
muqimjon <muqimjon.mr@gmail.com>
2025-10-20 21:06:22 +0500

    cleans trash codes

--- VoltStream/src/modules/Discovery/Models/ServiceRegistration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Discovery.Models;
-
-public class ServiceRegistration
-{
-    public string ServiceId { get; set; } = default!;
-    public string IpAddress { get; set; } = default!;
-    public int Port { get; set; }
-    public DateTime RegisteredAt { get; set; } = DateTime.UtcNow;
-}
