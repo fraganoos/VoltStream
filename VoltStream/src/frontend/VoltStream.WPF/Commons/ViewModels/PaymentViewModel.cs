918bc5d8dfd71373ff4686f1a47548af253819c5
Yakhya <yahyo1976@gmail.com>
2025-10-23 19:59:03 +0500

    Payment corrected

--- VoltStream/src/frontend/VoltStream.WPF/Commons/ViewModels/PaymentViewModel.cs
+++ VoltStream/src/frontend/VoltStream.WPF/Commons/ViewModels/PaymentViewModel.cs
@@ -2,6 +2,7 @@
 
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Messaging;
+using System.Windows;
 using VoltStream.WPF.Commons.Messages;
 
 public partial class PaymentViewModel : ViewModelBase
@@ -11,6 +12,7 @@ public partial class PaymentViewModel : ViewModelBase
     [ObservableProperty] private decimal amount; // orginal kirim summa
     [ObservableProperty] private decimal exchangeRate; //dollar kurs yoki pul o'tkazish foizi
     [ObservableProperty] private decimal netAmount; // qarz summadan ochiraldigan summa
+    [ObservableProperty] private decimal? discount; // chegirma
     [ObservableProperty] private string description = string.Empty;
     [ObservableProperty] private long currencyId;
     [ObservableProperty] private long customerId;
@@ -19,14 +21,18 @@ public partial class PaymentViewModel : ViewModelBase
     [ObservableProperty] private decimal? incomeAmount;
     [ObservableProperty] private decimal? expenseAmount;
 
-    [ObservableProperty] private decimal lastBalance;
-    [ObservableProperty] private decimal balance;
+    [ObservableProperty] private decimal? lastBalance;
+    [ObservableProperty] private decimal? balance;
 
     // UI control properties
     [ObservableProperty] private bool isIncomeEnabled = true;
     [ObservableProperty] private bool isExpenseEnabled = true;
 
-
+    partial void OnExchangeRateChanged(decimal value)
+    {
+        ReCalculateIncome();
+        ReCalculateExpense();
+    }
     partial void OnIncomeAmountChanged(decimal? value)
     {
         ReCalculateIncome();
@@ -38,10 +44,9 @@ public partial class PaymentViewModel : ViewModelBase
         else
         {
             IsExpenseEnabled = false;
-            expenseAmount = null;
+            WeakReferenceMessenger.Default.Send(new FocusRequestMessage("Discription"));
         }
     }
-
     partial void OnExpenseAmountChanged(decimal? value)
     {
         ReCalculateExpense();
@@ -53,11 +58,11 @@ public partial class PaymentViewModel : ViewModelBase
         else
         {
             IsIncomeEnabled = false;
-            incomeAmount = null;
         }
     }
 
-    private void ReCalculateIncome()
+
+    public void ReCalculateIncome()
     {
         if (IncomeAmount.HasValue && IncomeAmount != 0)
         {
@@ -73,7 +78,7 @@ public partial class PaymentViewModel : ViewModelBase
         }
     }
 
-    private void ReCalculateExpense()
+    public void ReCalculateExpense()
     {
         if (ExpenseAmount.HasValue && ExpenseAmount != 0)
         {
