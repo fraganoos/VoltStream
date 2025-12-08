4fbd5e04b2b29b4d1f23c43bac8b9ca0ac42fec0
yahyo1976@gmail.com <yahyo1976@gmail.com>
2025-12-08 16:57:56 +0500

    Discription

--- src/backend/VoltStream.Infrastructure/Migrations/AppDbContextModelSnapshot.cs
+++ src/backend/VoltStream.Infrastructure/Migrations/AppDbContextModelSnapshot.cs
@@ -2,21 +2,18 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using VoltStream.Infrastructure.Persistence;
 
 #nullable disable
 
-namespace VoltStream.Infrastructure.Persistence.Migrations
+namespace VoltStream.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20251205171147_InitialMigrate")]
-    partial class InitialMigrate
+    partial class AppDbContextModelSnapshot : ModelSnapshot
     {
-        /// <inheritdoc />
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
